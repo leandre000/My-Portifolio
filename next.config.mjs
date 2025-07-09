a43d84283efae9668fefcce5b7aa12b6a296ff68
Dieudonne000 <munezadieudonne2021@gmail.com>
2025-07-09 20:39:49 +0200

    modify-config

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,13 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({ canvas: 'commonjs canvas' });
+    }
+    return config;
+  },
 }
 
 export default nextConfig
